170f9566b0737054e6b8b4125a7a6b96ff9f3ebc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to insert a table into the header section
+
+
+<p>This example illustrates how to insert a table into a document's header area.</p>
+
+<br/>
+
+
---
+++ VB/Form1.vb
@@ -0,0 +1,20 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraRichEdit.API.Native
+
+Namespace RichHeaderTable
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim section As Section = richEditControl1.Document.Sections(0)
+			Dim subdoc As SubDocument = section.BeginUpdateHeader()
+			subdoc.Tables.Add(subdoc.Range.End, 2, 3, AutoFitBehaviorType.AutoFitToWindow)
+			section.EndUpdateHeader(subdoc)
+		End Sub
+	End Class
+End Namespace
